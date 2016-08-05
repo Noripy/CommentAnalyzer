3e4641ee6d8292baf07ac370f9034ee20565de1b
Amitatatatata <popopo.badbad@docomo.ne.jp>
2016-08-05 12:08:25 +0900

    MyNicoNico

--- MyNicoNico/src/main/WakuInformation.java
+++ MyNicoNico/src/main/WakuInformation.java
@@ -5,4 +5,19 @@ public class WakuInformation {
 	String addr = null;
 	String port = null;
 	String thread = null;
+	
+	public WakuInformation(){}
+	
+	public WakuInformation(String room, String addr, String port, String thread){
+		this.room = room;
+		this.addr = addr;
+		this.port = port;
+		this.thread = thread;
+	}
+	
+	public WakuInformation(String addr, String port, String thread){
+		this.addr = addr;
+		this.port = port;
+		this.thread = thread;
+	}
 }
